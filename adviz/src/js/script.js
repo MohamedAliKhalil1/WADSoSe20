d6267f3a3b62f7537d4d1cb353b6c7c3472fbc2e
Mohamed Ali <mohamedalikahlil5@gmail.com>
2020-05-12 12:32:23 +0200

    js and some css

---
+++ adviz/src/js/script.js
@@ -0,0 +1,9 @@
+function showHandler(){
+    document.getElementById('show').style.display = 'block';
+    document.getElementById('addadress-holder').style.display = 'none';
+}
+
+function addHandler() {
+    document.getElementById('show').style.display = 'none';
+    document.getElementById('addadress-holder').style.display = 'grid';
+}
