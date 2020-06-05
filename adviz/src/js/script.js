717e5a34a580ba6378a5b95fe5e8507593efc5a6
Mohamed Ali <mohamedalikahlil5@gmail.com>
2020-06-05 22:40:44 +0200

    adding mobile responsive

--- adviz/src/js/script.js
+++ adviz/src/js/script.js
@@ -2,6 +2,14 @@
 import {PageControl} from './pagecontrol.js';
 
 
+    document.getElementById("trigram").addEventListener("click", function(){
+        let x = document.getElementById("drop_down_click");
+        if(x.className === "nav_bar_links"){
+            x.className += " responsive";
+        } else {
+            x.className = "nav_bar_links";
+        }
+    });
 
 
 
