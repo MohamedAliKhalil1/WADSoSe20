9e1775c7049cf97ed172951b750b081a5f084cdc
Aiman Abdou <aiman.i.abdou@gmail.com>
2020-06-06 21:30:41 +0200

    Change Log - Added Google Map iFrame and Contact List Container into the new Design. - script.js: Added iFrame for Google Maps and changed the parameters of the included link to change the pinned address. - map.js: Created empty map.js file.

--- adviz/src/js/script.js
+++ adviz/src/js/script.js
@@ -11,15 +11,28 @@ import {PageControl} from './pagecontrol.js';
         }
     });
 
+{/* <iframe scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Los+Angeles+Hollywood+blvd+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="1200" height="600" frameborder="0"></iframe> */}
+
+let street = "Wilmersdorfer Str. 10";
+let city = "Berlin";
+let zipcode = "10585"; 
 
 
 
 
 
 
+// let locationString = "Los+Angeles+Hollywood+blvd";
+let locationString = street+"+"+city+"+"+zipcode;
+// let locationString = "Los+Angeles+Hollywood+blvd"; 
+// document.getElementById("mapContainer").innerHTML = '<iframe class="googleMapIframe" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=480&amp;height=400&amp;hl=en&amp;q=%20' + locationString + '+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="1200" height="600" frameborder="0"></iframe>';
+document.getElementById("mapContainer").innerHTML = '<iframe class="googleMapIframe" scrolling="no" src="https://maps.google.com/maps?width=480&amp;height=400&amp;hl=en&amp;q=%20' + locationString + '+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0"></iframe>';
 
 
 
+
+let pageControlDebug = new PageControl();
+pageControlDebug.showMainContent();
 
 // function showHandler(){
 //     document.getElementById('show').style.display = 'block';
